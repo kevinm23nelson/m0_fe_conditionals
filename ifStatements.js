07b0351c85cbc5e5b27376abea5de97e6e7e4b4c
Kevin Nelson <km.nelson@outlook.com>
2024-03-15 21:18:43 -0400

    Added If Statements Assignment

--- ifStatements.js
+++ ifStatements.js
@@ -9,10 +9,11 @@
 // Experiment with manipulating the value held within numQuarters to make sure both conditions can be achieved.
 
 var numQuarters = 0;
-
-console.log("I have enough money for a gumball");
-console.log("I don't have enough money for a gumball");
-
+if (numQuarters >= 2) {
+    console.log("I have enough money for a gumball");
+} else {
+    console.log("I don't have enough money for a gumball");
+}
 
 // CHALLENGE 2
 // Using the variables defined below, write code that will tell you if you have the ingredients to make a pizza. 
@@ -28,4 +29,8 @@ console.log("I don't have enough money for a gumball");
 
 var cupsOfFlour = 1;
 var hasSauce = true;
-
+if (cupsOfFlour >= 2 && hasSauce === true) {
+    console.log("I can make pizza.");
+} else {
+    console.log("I cannot make pizza");
+}
