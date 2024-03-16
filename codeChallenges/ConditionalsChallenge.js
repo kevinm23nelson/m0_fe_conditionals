a78741073710d9e8b6347ced78639a3c2995aaa6
Kevin Nelson <km.nelson@outlook.com>
2024-03-15 21:14:02 -0400

    Added ConditionalsChallenge

---
+++ codeChallenges/ConditionalsChallenge.js
@@ -0,0 +1,10 @@
+//Write a JavaScript program that defines a variable that stores a Number. 
+//The program should print out the String "even" if the Number is even, and the String "odd" if the Number is odd. 
+//Hint: You may need to do some extra research on the remainder operator (%) to solve this challenge.
+
+number = 17
+if (number & 1 == 1) {
+    console.log("odd");
+} else {
+    console.log("even");
+}
